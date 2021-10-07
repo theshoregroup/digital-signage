ab17168c1cd6c514f1268f0d2c3748a961a735b0
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2021-10-07 12:51:08 +0000

    Youtube test implemented

---
+++ pages/youtube_test.js
@@ -0,0 +1,4 @@
+//u toob
+export default function youtube_test() {
+return <iframe width="560" height="315" src="https://www.youtube.com/embed/9Auq9mYxFEE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+}
