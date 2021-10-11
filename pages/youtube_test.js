d04dc7623701ebd6f35eda52d03abcd73a24ec7a
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2021-10-11 15:08:28 +0000

    finn

--- pages/youtube_test.js
+++ pages/youtube_test.js
@@ -1,4 +1,11 @@
-//u toob
+
+
 export default function youtube_test() {
-return <iframe width="560" height="315" src="https://www.youtube.com/embed/9Auq9mYxFEE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+return <parent>
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/9Auq9mYxFEE?cc_load_policy=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+ 
+ <iframe src="//rss.bloople.net/?url=https%3A%2F%2Ffeeds.skynews.com%2Ffeeds%2Frss%2Fuk.xml&limit=10&showtitle=false&type=html"></iframe>
+</parent>
 }
+
+
