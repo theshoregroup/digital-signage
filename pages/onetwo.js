860d36983b4bb7d394a3d5f61ac7b5d05c614a55
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2021-10-14 09:55:59 +0100

    Commit

--- pages/onetwo.js
+++ pages/onetwo.js
@@ -2,35 +2,69 @@ let Parser = require('rss-parser');
 let parser = new Parser();
 import Head from 'next/head';
 
-import Post from '../components/post'
-
 export async function getStaticProps() {
     // fetch list of posts
     const RSSfeed = await parser.parseURL('http://feeds.skynews.com/feeds/rss/uk.xml')
-    
+    let url = 'http://api.weatherapi.com/v1/current.json?key=ac517e0edf3142a6ae282635211410&q=BN1&aqi=no';
+    const weather = await (await fetch(url)).json();
+
     return {
       props: {
         RSSfeed,
+        weather,
       },
     }
   }
-  
 
-  export default function IndexPage({ RSSfeed }) {
+  if (typeof window !== "undefined") {
+  (function () {
+
+    var clockElement = document.getElementById( "clock" );
+  
+    function updateClock ( clock ) {
+      clock.innerHTML = new Date().toLocaleTimeString();
+    }
+  
+    setInterval(function () {
+        updateClock( clockElement );
+    }, 1000);
+  
+  }());
+  }
+  
+  export default function IndexPage({ RSSfeed, weather, clock }) {
     return (
       <main>
         <Head>
           <title>Home page</title>
         </Head>
+        <section>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/9Auq9mYxFEE?cc_load_policy=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </section>
   
         <h1>List of posts</h1>
+        <section>
+          {RSSfeed.description}
+          {RSSfeed.title}
+        </section>
   
         <section>
-          {RSSfeed.items.map((post) => (
-            <Post {...post} key={post.id} />
-          ))}
+          <div class="container">
+            <h1 class="heading"> Weather </h1>
+          {weather.location.name}
+          {weather.current.condition.text}
+          {weather.current.temp_c}°C
+   
+  
+          </div>
+        </section>
+
+        <section>
+        <span id="clock"></span>
         </section>
       </main>
-    )
-  }
-  
+)
+}
+
+    
+
