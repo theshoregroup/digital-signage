06e175f61e75f62b370a87ceee41a215daca05ad
Liam Doyle <limegorilla@gmail.com>
2021-10-28 12:59:14 +0100

    Remove Google fonts from demo

---
+++ pages/demo.js
@@ -0,0 +1,86 @@
+import Parser from 'rss-parser'
+import Head from 'next/head'
+import Image from 'next/image'
+import Clock from '../components/Clock'
+import Post from '../components/CompanyPost'
+import SkyNews from '../components/skynews'
+import Weather from '../components/Weather'
+import YouTubePlayer from '../components/youtube'
+import { ServerTalk } from '../components/servertalk'
+import CompanyPost from '../components/CompanyPost'
+import Marquee from 'react-fast-marquee'
+
+export async function getStaticProps() {
+    // RSS Feed - Sky News
+    let parser = new Parser();
+    const RSSfeed = await parser.parseURL('http://feeds.skynews.com/feeds/rss/uk.xml')
+
+    // Weather
+    let weatherUrl = 'http://api.weatherapi.com/v1/current.json?key=ac517e0edf3142a6ae282635211410&q=BN1&aqi=no';
+    const weather = await (await fetch(weatherUrl)).json();
+
+    // Company Posts
+    const posts = await ServerTalk('/items/posts', process.env.BACKEND_USER_TOKEN)
+    return {
+        props: {
+            RSSfeed,
+            weather,
+            posts
+        },
+    }
+}
+
+export default function IndexPage({ RSSfeed, weather, posts }) {
+
+    return (
+        <main>
+
+
+            <Head>
+                <title>Home page</title>
+            </Head>
+
+            <div className="bg-opacity-50 bg-gray-300 static m-2 h-48 font-Ubuntu p-3">
+                <div className="bg-gray-300 absolute top-0 left-0 p-8">
+                    {/*
+            Youtube Player
+            */}
+                    <YouTubePlayer query="9Auq9mYxFEE" params="?autoplay=1&mute=1&cc_load_policy=1&disablekb=1&fs=0&modestBranding=1" />
+                </div>
+
+
+                <div className="bg-gray-300 absolute top-0 right-0 p-16">
+                    <Image src="/shoreLogo.png" alt="Logo" width="540" height="246" />
+                    <h1> [COMPANY NEWS AND ANNOUNCEMENTS HERE]</h1>
+
+                </div>
+                <div className="text-3xl absolute bottom-0 left-0 p-2">
+                    <Marquee gradientColor speed="75
+          ">
+                        {RSSfeed.items.map((title, index) => (
+                            <SkyNews {...title} id={index} />
+                        ))}
+                    </Marquee>
+                </div>
+                <div className="flex flex-row absolute bottom-0 left-0 p-2">
+                    <Weather {...weather} />
+                    <div className="text-3xl p-5">
+                        <Clock />
+
+                    </div>
+
+                </div>
+            </div>
+
+        </main>
+
+
+
+    )
+}
+
+
+
+
+
+
