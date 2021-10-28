1310e6bb1b9c97717e08c4ece83fab80a2b107f8
Liam Doyle <limegorilla@gmail.com>
2021-10-28 12:55:50 +0100

    Remove console logging in Company Posts

--- components/CompanyPost.js
+++ components/CompanyPost.js
@@ -1,6 +1,5 @@
 
 export default function CompanyPost({ title, body, author, authorThumb, datePosted }) {
-  console.log(title, body, author, authorThumb, datePosted)
   return (
     <>
       <h2>{title}</h2>
