bf12da2ad6ce23d19318aff4c10e4b35fd5d638c
Liam Doyle <limegorilla@gmail.com>
2021-11-17 16:41:22 +0000

    Added electron-updater & initial electron-builder setup
    + Added electron-updater as a dependancy
    + Added electron-builder.yml with initial builder config (WIP)
    * Changed public/electron.js with a reference to electron-updater
    + Changed package.json with new electron-builder scripts (WIP)
    
    See: https://blog.borodutch.com/how-to-deploy-an-electron-app-with-auto-updates-enabled-for-free-in-2021/

--- public/electron.js
+++ public/electron.js
@@ -1,16 +1,26 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const path = require("path");
-const isDev = require("electron-is-dev");
+import { app, BrowserWindow } from "electron";
+import { autoUpdater } from 'electron-updater'
+import path from "path";
+import isDev from "electron-is-dev";
+
+
 let mainWindow;
+
+
 function createWindow() {
-    mainWindow = new BrowserWindow({ width: 900, height: 680 });
+    mainWindow = new BrowserWindow({
+        width: 900,
+        height: 680
+    });
+
     mainWindow.loadURL(
         isDev
             ? "http://localhost:3000"
             : `file://${path.join(__dirname, "../build/index.html")}`
     );
+
+    autoUpdater.checkForUpdatesAndNotify()
+
     mainWindow.on("closed", () => (mainWindow = null));
 }
 app.on("ready", createWindow);
