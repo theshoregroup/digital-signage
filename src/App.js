c049be96f54276a61e16e5d352e9fef7685500fe
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2021-11-19 09:29:27 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import Weather from "./components/Weather";
 import YouTubePlayer from "./components/YouTubePlayer";
 import Clock from "./components/Clock";
 import "./App.css";
+import logo from "./images/shoreLogo.png";
 import Marquee from "react-fast-marquee";
-//import Skynews from './components/Skynews';
-  
-
+import SkyNews from "./components/Skynews";
 
 function App() {
-  
-
   return (
-
     <div>
       <div className="absolute top-0 right-0 center p-9 shadow-md ">
         <YouTubePlayer
@@ -19,22 +15,20 @@ function App() {
           params="?autoplay=1&mute=1&cc_load_policy=1&disablekb=1&fs=0&modestBranding=1"
         />
       </div>
-      
+
       <div className="absolute top-0 right-0 opacity-70 p-10 ">
-        <img src="/public/logo.png" alt="" />
+        <img src={logo} alt="" />
       </div>
       <div className="absolute bottom-0 right-0 text-7xl p-6 shadow-md">
         <Clock />
       </div>
-      <div className="absolute top-0 left-20 text-4xl p-14 shadow-md bg-gradient-to-br from-green-400 to-cyan-500">
-        <Weather/>
+      <div className="absolute top-0 left-20 ">
+        <Weather />
       </div>
       <div className="absolute bottom-0 text-6xl p-6 shadow-md">
         <Marquee gradientColor speed="90">
-            (MARQUEE DOESNT WORK)
+          <SkyNews/>
         </Marquee>
-        
-        
       </div>
     </div>
   );
