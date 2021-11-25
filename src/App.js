88695eae64c5dfc121541301cc3f86f2c8183ea7
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2021-11-25 16:05:52 +0000

    Implemented Marquee + began dynamic view implementation

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import Weather from "./components/Weather";
-import YouTubePlayer from "./components/YouTubePlayer";
 import Clock from "./components/Clock";
-import "./App.css";
+//import "./App.css";
 import logo from "./images/shoreLogo.png";
-import Marquee from 'react-double-marquee';
-import News from './components/News'
+import News from "./components/News";
+import RenderView from "./components/RenderView"
+
 
 
 function App() {
   return (
     <div>
       <div className="absolute top-0 right-0 center p-9 shadow-md ">
-        <YouTubePlayer
-          query="9Auq9mYxFEE"
-          params="?autoplay=1&mute=1&cc_load_policy=1&disablekb=1&fs=0&modestBranding=1"
-        />
+        <RenderView/>
       </div>
 
       <div className="absolute top-0 right-0 opacity-70 p-10 ">
         <img src={logo} alt="" />
       </div>
-      <div className="absolute bottom-0 right-0 text-7xl p-6 shadow-md">
+      <div className="absolute bottom-20 right-0 text-7xl ">
         <Clock />
       </div>
       <div className="absolute top-0 left-20 ">
         <Weather />
       </div>
-      <div className="bottom-0 left-0">
-        <Marquee>
-          <News/>
-        </Marquee>
-
+      <div className= "absolute bottom-0 left-0 text-6xl w-screen h-auto">
+        <News/>
       </div>
     </div>
   );
