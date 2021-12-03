5d110600d8db1d276fb78ad54e36386676897dc1
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2021-12-03 16:45:49 +0000

    Changed theme

--- src/components/YouTubePlayer.js
+++ src/components/YouTubePlayer.js
@@ -9,7 +9,7 @@ function YouTubePlayer({ query, params }) {
     <iframe
       width="1350"
       height="759"
-      src="https://www.youtube.com/embed/9Auq9mYxFEEcc_load_policy=1&cc_lang_pref=en"
+      src="https://www.youtube.com/embed/9Auq9mYxFEE"
       title="YouTube video player"
       frameborder="0"
       allow="acce
