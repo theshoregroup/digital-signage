38a991ee2078e575c64fad5338ded73ea888c6fd
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2021-12-03 10:21:01 +0000

    Reverting to previous version

--- src/components/RenderView.js
+++ src/components/RenderView.js
@@ -1,9 +1,9 @@
 import CompanyPost from './CompanyPost';
 import YouTubePlayer from "./YouTubePlayer";
-import React, { useState } from 'react';
 
 
 function RenderView(render) {
+<<<<<<< HEAD
     function renderTime() {
         render = (render == "cp" ? "yt" : "cp")
         }
@@ -11,24 +11,26 @@ function RenderView(render) {
   
 
     switch (render) {
+=======
+    setInterval(render, 5000);
+    render = "yt"
+    switch(render) {
+>>>>>>> parent of ca5c410 (Implemented switching view (its broken though))
         case 'yt':
             return (
-                <YouTubePlayer />
+                <YouTubePlayer/>
             )
         case 'cp':
             return (
-                <CompanyPost />
+                <CompanyPost/>
             )
         default:
-            return (
-                <YouTubePlayer />
+            return(
+                <YouTubePlayer/>
             )
 
     }
 }
-
-
-
 
 
 
