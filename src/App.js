6ccff8941a6b52e370611e279a0126846a5137c0
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2021-12-06 16:28:35 +0000

    Updated weather

--- src/App.js
+++ src/App.js
@@ -8,17 +8,22 @@ import Marquee from "react-fast-marquee";
 
 function App() {
   return (
-    <div className="max-w-screen max-h-screen grid grid-cols-9 grid-rows-9 max-w-screen p-10 gap-10 bg-shoreblue-500">
+    <div className="max-w-screen max-h-screen grid grid-cols-9 grid-rows-9 min-w-screen min-h-screen p-10 gap-10 bg-shoreblue-500">
       <div className="col-span-2 row-span-1 text-white ">
         <img src={logo} alt="" />
       </div>
       <div className="col-span-7 row-span-3 text-7xl gap-3 bg-white  max-w-auto max-h-16 ">
         <RenderView />
-        <Clock />
-        <News />
+     
       </div>
       <div className="col-span-2 row-span-7 text-6xl  ">
         <Weather />
+      </div>
+      <div className="col-span-3 row-span-7 text-8xl  ">
+        <Clock />
+      </div>
+      <div className="col-span-6 row-span-7 text-8xl  ">
+      <News />
       </div>
     </div>
   );
