1dcb2b20511d1fed16f3c9c2d96b78692f063d65
Liam Doyle <limegorilla@gmail.com>
2021-12-16 14:57:47 +0000

    New base components

---
+++ src/components/base/Bottom.js
@@ -0,0 +1,30 @@
+import Marquee from "../subcomponents/Marquee"
+
+export default function Bottom(props) {
+    // Define props
+    const elementType = props.elementType || 'marquee' // i.e 'marquee' | This should eventially use JS React Use State to be able to change on-fly
+    const elementContent = props.content || {
+        // This is passed through to the subcomponent.
+        // This is an example used for testing, remove for release.
+        'type': 'text',
+        'data': [
+            'Hello!',
+            'This is a marquee!',
+            'Each element is seperated.'
+        ]
+    }
+
+    
+
+
+    switch (elementType) {
+        case 'marquee':
+            // This will display ../functional/Marquee.js
+            // elementContent should be passed through to this (refrenced above)
+
+            return <Marquee type={elementContent.type} content={elementContent.data} />
+        default:
+            return <div>No element selected</div>
+    }
+
+}
---
+++ src/components/base/Main.js
@@ -0,0 +1,36 @@
+// Main.JS will have three states:
+// 1. left - means it will be rendered in left side of the screen
+// 2. right - means it will be rendered in right side of the screen
+// default - means it will be rendered as the largest element overtop of everything else
+
+export default function MainComponent(props) {
+    // Currently just a switch statement to choose what 'version' of the component to render
+    switch (props.state) {
+        case 'left':
+            return (
+                <div className="h-full w-full">
+                    <iframe
+                        className="h-full w-full"
+                        src="https://www.youtube.com/embed/9Auq9mYxFEE?autohide=1&autoplay=1&cc_load_policy=1&mute=1"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                        autoplay="True"
+
+
+                    />
+                </div>
+            );
+        case 'right':
+            return (
+                <div className="absolute h-screen w-screen">
+                </div>
+            );
+        default:
+            return (
+                <div className="absolute h-screen w-screen">
+                </div>
+            );
+    }
+}
