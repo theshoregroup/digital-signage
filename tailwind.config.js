8286e5e581999b22b5662a99b25b39dab5c17262
Liam Doyle <limegorilla@gmail.com>
2022-01-04 10:08:58 +0000

    Update to Tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend:
