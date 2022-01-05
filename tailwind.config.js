8420ec1b248ece97557302fad9671fcf87d85b0c
Liam Doyle <limegorilla@gmail.com>
2022-01-05 10:22:50 +0000

    Finish header styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend:
-    {
-      colors: {
-        shoreblue: {
-          "50": "#696caa",
-          "100": "#5f62a0",
-          "200": "#555896",
-          "300": "#4b4e8c",
-          "400": "#414482",
-          "500": "#373a78",
-          "600": "#2d306e",
-          "700": "#232664",
-          "800": "#191c5a",
-          "900": "#0f1250"
-        }
-      }
+    fontFamily: {
+      mono: ['IBM Plex Mono', 'Menlo', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier', 'monospace' ],
+      sans: ['IBM Plex Sans', 'Helvetica Neue', 'Arial', 'sans-serif' ],
+      serif: ['IBM Plex Serif', 'Georgia', 'Times', 'serif']
     },
+    extend: {},
   variants: {
     extend: {},
   },
