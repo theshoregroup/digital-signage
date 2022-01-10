ca40fc53dce7c3116366a9b6cbd811afe4cbbbde
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-01-10 16:11:38 +0000

    Added dummy JSON arrays so app starts correctly
    Remove later and implement proper loading function. Noticed drastic decrease in performance

--- src/components/API.js
+++ src/components/API.js
@@ -1,7 +1,34 @@
 import { useEffect, useState } from "react";
 
 export const Compost = () => {
-  const [apiData, setAPIData] = useState({});
+  const [apiData, setAPIData] = useState({
+    "data": [
+        {
+            "id": 9,
+            "status": "published",
+            "sort": null,
+            "user_created": "b2f328f3-c74d-4ffd-bbf7-6d0db5d021c0",
+            "date_created": "2022-01-07T13:56:32.000Z",
+            "user_updated": "b2f328f3-c74d-4ffd-bbf7-6d0db5d021c0",
+            "date_updated": "2022-01-07T16:07:28.000Z",
+            "title": "version-1",
+            "body": "Version 1.0 of the Shore Group Screens App!\n\n\n You can use a handy web app to write basically anything you'd like on these screens.\n\n I'm sure this power will be used responsibly.\n \n \n \n Big up the mainframe massive",
+            "availability": "internal"
+        },
+        {
+            "id": 10,
+            "status": "published",
+            "sort": null,
+            "user_created": "b2f328f3-c74d-4ffd-bbf7-6d0db5d021c0",
+            "date_created": "2022-01-10T11:21:32.000Z",
+            "user_updated": "b2f328f3-c74d-4ffd-bbf7-6d0db5d021c0",
+            "date_updated": "",
+            "title": "",
+            "body": "",
+            "availability": ""
+        }
+    ]
+});
   useEffect(() => {
     const getDataFromApi = async () => {
       const response = await fetch(
@@ -11,18 +38,25 @@ export const Compost = () => {
       console.log("json", responseJson);
       setAPIData(responseJson);
     };
-    setInterval(getDataFromApi, 5000)
+    setInterval(getDataFromApi, 1500)
+   
   }, []);
+
+
 
   return (
     <div>
-       <div className="text-4xl font-semibold">
-       <h6>{apiData?.data[3]?.title}</h6>
-       </div>
-       <div className="text-2xl">
-       <h6>{apiData?.data[3]?.body}</h6>
-       </div>
-    </div>
+    <div className="text-5xl font-semibold">
+     <h6>{apiData?.data[1]?.title}</h6>
+     </div>
+     <div className="text-2xl">
+     <h6>{apiData?.data[1]?.body}</h6>
+     </div>
+     <div className="text-xl font-semibold text-center-left">
+       <h6>-{apiData?.data[1].posted_by}</h6>
+     </div>
+     </div>
+    
   );
 };
 
--- src/components/tempNews.js
+++ src/components/tempNews.js
@@ -2,42 +2,49 @@ import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
 
 const News = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({
+    "status": "ok",
+    "totalResults": 70,
+    "articles": [
+        {
+            "source": {
+                "id": "google-news",
+                "name": "Google News"
+            },
+            "author": null,
+            "title": "Italian media highlight Mourinho's errors and Maitland-Niles' shocking Roma debut - Football Italia - Football Italia",
+            "description": null,
+            "url": "https://news.google.com/__i/rss/rd/articles/CBMibGh0dHBzOi8vZm9vdGJhbGwtaXRhbGlhLm5ldC9pdGFsaWFuLW1lZGlhLWhpZ2hsaWdodC1tb3VyaW5ob3MtZXJyb3JzLWFuZC1tYWl0bGFuZC1uaWxlcy1zaG9ja2luZy1yb21hLWRlYnV0L9IBAA?oc=5",
+            "urlToImage": null,
+            "publishedAt": "2022-01-10T10:18:30Z",
+            "content": null
+        },
+    ]
+});
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    const getNewsFromApi = async () => {
+    setLoading(true);
+    (async function getNewsFromApi(){
       const response = await fetch(
         "https://newsapi.org/v2/top-headlines?country=gb&category=sports&apiKey=1696bf49a11542e786afeb06b1540cf9"
       );
       const responseJson = await response.json();
       console.log("json", responseJson);
       setData(responseJson);
-    };
-    setInterval(getNewsFromApi, 10000)
+    })();
+    setLoading(false);
   }, []);
 
-
+  if (loading) {
+    return <h1> Data is loading...</h1>
+  }
   return (
     <div >
-    
       <Marquee gradientColor="" speed="120">
-        <h1>{data?.articles[0].title} </h1>
-        <h1>{data?.articles[1].title} </h1>
-        <h1>{data?.articles[2].title} </h1>
-        <h1>{data?.articles[3].title} </h1>
-        <h1>{data?.articles[4].title} </h1>
-        <h1>{data?.articles[5].title} </h1>
-        <h1>{data?.articles[6].title} </h1>
-        <h1>{data?.articles[7].title} </h1>
-        <h1>{data?.articles[8].title} </h1>
-
-      
+      <h1>{data.articles[0].title}</h1>  
       </Marquee>
     </div>
-
-
   )
 }
 
 export default News
-
-/* */
