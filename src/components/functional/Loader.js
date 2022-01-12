cc999196e9d1b2368314dfe78c8460c4eb3a4d44
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-01-12 19:39:57 +0000

    Updated loader
    Pretty colours :)

--- src/components/functional/Loader.js
+++ src/components/functional/Loader.js
@@ -1,7 +1,8 @@
 export const Loader = () => (
     <div class="flex items-center justify-center ">
-        <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin"></div>
+        <div class="w-40 h-40 border-t-8 border-r-8 border-b-8 border-t-green-300 border-r-blue-500 border-b-purple-500 rounded-full animate-spin"></div>
     </div>
     
     );
 
+  
