e11d4e8708cca597f8a7513ce7a360d5d5fefa87
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-01-12 19:29:29 +0000

    Updated loader function
    Now uses Tailwind

---
+++ src/components/functional/Loader.js
@@ -0,0 +1,7 @@
+export const Loader = () => (
+    <div class="flex items-center justify-center ">
+        <div class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin"></div>
+    </div>
+    
+    );
+
