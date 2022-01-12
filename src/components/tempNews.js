baa33d0bc262349b98fba65f013135f4dd3a6cf5
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-01-12 19:39:57 +0000

    Updated loader
    Pretty colours :)

--- src/components/tempNews.js
+++ src/components/tempNews.js
@@ -1,31 +1,9 @@
 import axios from "axios";
-
+import { Loader } from "./functional/Loader"
 import React from "react";
 
 
-const Loader = () => (
-  <div class="divLoader">
-    <svg class="svgLoader" viewBox="0 0 100 100" width="10em" height="10em">
-      <path
-        stroke="none"
-        d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50"
-        fill="#51CACC"
-        transform="rotate(179.719 50 51)"
-      >
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          calcMode="linear"
-          values="0 50 51;360 50 51"
-          keyTimes="0;1"
-          dur="1s"
-          begin="0s"
-          repeatCount="indefinite"
-        ></animateTransform>
-      </path>
-    </svg>
-  </div>
-);
+
 
 class News extends React.Component {
   constructor(props) {
