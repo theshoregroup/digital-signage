287eb5dce199c7401545ea2afc63e8798cd679b1
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-01-14 15:32:11 +0000

    Added excel compatibility
    WOOOHOOOOOOOOO

--- src/components/API.js
+++ src/components/API.js
@@ -27,17 +27,50 @@ class Compost extends React.Component {
     if (this.state.loading) {
       return <Loader />;
     } else {
-      let specificBody = this.state.data.data[1].body;
-      let specificTitle = this.state.data.data[1].title;
-      let specificPosted_By = this.state.data.data[1].posted_by;
+      let apiSwitcher
+      switch  (apiSwitcher){      //load API data into arrays so specific elements can be rendered. Janky method. Re-visit.
+      //
+      case 'element0':
+      let body0 = this.state.data.data[0].body;
+      let title0 = this.state.data.data[0].title;
+      let posted_By0 = this.state.data.data[0].posted_by;
+      //
+      case 'element1':
+      let body1 = this.state.data.data[1].body;
+      let title1 = this.state.data.data[1].title;
+      let posted_By1 = this.state.data.data[1].posted_by;
+      //
+      case 'element2':
+      let body2 = this.state.data.data[2].body;
+      let title2 = this.state.data.data[2].title;
+      let posted_By2 = this.state.data.data[2].posted_by;
+      //
+      case 'element3':
+      let body3 = this.state.data.data[3].body;
+      let title3 = this.state.data.data[3].title;
+      let posted_By3 = this.state.data.data[3].posted_by;
+      //
+      case 'element4':
+      let body4 = this.state.data.data[4].body;
+      let title4 = this.state.data.data[4].title;
+      let posted_By4 = this.state.data.data[4].posted_by;
+      //
+      case 'element5':
+      let body5 = this.state.data.data[5].body;
+      let title5 = this.state.data.data[5].title;
+      let posted_By5 = this.state.data.data[5].posted_by;
+      
+      
+
 
       return (
         <div className="text-center">
-          <div className="font-semibold text-4xl">{specificTitle}</div>
-          <div className="text-2xl"> {specificBody}</div>
-          <div className="text-3xl font-semibold">-{specificPosted_By}</div>
+          <div className="font-semibold text-4xl">{apiSwitcher}</div>
+          <div className="text-2xl"> {body2}</div>
+          <div className="text-3xl font-semibold">-{posted_By1}</div>
         </div>
       );
+      }
     }
   }
 }
