15ff094fb8151745fe20f83c55d6905c4a402fd5
Liam Doyle <limegorilla@gmail.com>
2022-01-19 11:30:07 +0000

    Test release

--- public/electron.js
+++ public/electron.js
@@ -7,10 +7,27 @@ const { app, BrowserWindow } = require("electron");
 const { autoUpdater } = require('electron-updater')
 const path = require("path")
 const isDev = require("electron-is-dev")
+const log = require("electron-log");
 
+//-------------------------------------------------------------------
+// Logging
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This logging setup is not required for auto-updates to work,
+// but it sure makes debugging easier :)
+//-------------------------------------------------------------------
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 let mainWindow;
 
+function sendStatusToWindow(text) {
+    log.info(text);
+    console.log('msg: ' + text);
+    // win.webContents.send('message', text);
+}
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -26,8 +43,6 @@ function createWindow() {
             : `file://${path.join(__dirname, "../build/index.html")}`
     );
 
-    autoUpdater.checkForUpdatesAndNotify()
-
     mainWindow.on("closed", () => (mainWindow = null));
 }
 app.on("ready", createWindow);
@@ -40,4 +55,33 @@ app.on("activate", () => {
     if (mainWindow === null) {
         createWindow();
     }
+});
+
+// Automatic updates
+
+app.on('ready', function () {
+    autoUpdater.checkForUpdatesAndNotify();
+});
+
+autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+    autoUpdater.quitAndInstall();
 });
