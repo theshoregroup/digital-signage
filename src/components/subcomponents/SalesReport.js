2825694874bfe89d3f3cba8bd3dc12f19ccf4671
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-01-24 15:35:16 +0000

    Made text sizing/spacing more consistent

--- src/components/subcomponents/SalesReport.js
+++ src/components/subcomponents/SalesReport.js
@@ -2,8 +2,9 @@ let sales = "123,000"; //Make own component and editable from backend
 export default function SalesReport() {
   return (
     <div>
+        <h2 className="text-2xl text-center">Sales this week</h2>
       <h1 className="font-semibold text-6xl  ">£{sales}</h1>
-      <h2 className="text-2xl text-center">Sales this week</h2>
+      
     </div>
   );
 }
--- src/components/subcomponents/tempNews.js
+++ src/components/subcomponents/tempNews.js
@@ -3,7 +3,7 @@ import { Loader } from "../functional/Loader";
 import React from "react";
 
 
-
+let Source = "News" //Pull from API
 
 class News extends React.Component {
   constructor(props) {
@@ -29,6 +29,7 @@ class News extends React.Component {
       });
   }
 
+
   render() {
     if (this.state.loading) {
       return <Loader />
@@ -36,13 +37,16 @@ class News extends React.Component {
 
     else
       return (
-        // Disables ES-Lint warning about the use of <marquee>
-        // eslint-disable-next-line
+   
+        <div>
+        <h1>{Source} </h1>
+        
         <marquee className="space-x-2" scrollamount="10">
           {this.state.data.articles.map((item) => (
             <p className="inline text-5xl" key={item.key}>{item.title} </p>
           ))}
         </marquee>
+        </div>
       );
   }
 }
