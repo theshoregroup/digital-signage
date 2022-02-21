1468bf68db2abd37b6e2a906c6410a73766e0fa9
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-02-21 11:38:40 +0000

    Updated news scroller

--- src/components/subcomponents/tempNews.js
+++ src/components/subcomponents/tempNews.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 import { Loader } from "../functional/Loader";
 import React from "react";
-import { Index } from './Index'
+import { Index } from "./Index";
 
-
-let API_KEY="be5b5ec5bfa345388ac8db5692953db2"
-
-
-
+let API_KEY = "be5b5ec5bfa345388ac8db5692953db2";
 
 class News extends React.Component {
   constructor(props) {
@@ -18,50 +14,35 @@ class News extends React.Component {
     };
   }
 
-
-
-
-
-
-
   componentDidMount() {
-
- 
-
     axios
-      .get(
-        "https://newsapi.org/v2/top-headlines?country=gb&apiKey="+API_KEY
-      )
+      .get("https://newsapi.org/v2/top-headlines?country=gb&apiKey=" + API_KEY)
       .then((res) => {
-        var data = res.data
+        var data = res.data;
         this.setState({ data, loading: false });
-        console.log(data)
+        console.log(data);
       });
-
   }
 
   render() {
     if (this.state.loading) {
-      return <Loader />
-    }
+      return <Loader />;
+    } else //get articles titles as "headlines"
+      var headlines = this.state.data.articles.map((articles) => {
+        return articles.title
+      });
+  var join = headlines.join(' - ');  // "seperate headlines "
 
-    else
+    return (
+      <div>
 
-
-
-      return (
-
-        <div>
-          <h1>{this.state.data.articles[0].source.name} </h1>
-          {/* eslint-disable-next-line jsx-a11y/no-distracting-elements*/}
-          <marquee className="" scrollamount="10">
-            <p className="text-5xl"> {this.state.data.articles[0].title} </p>
-
-          </marquee>
-        </div>
-      );
+        {/* eslint-disable-next-line jsx-a11y/no-distracting-elements*/}
+        <marquee className="" scrollamount="10">
+          <p className="text-5xl"> {join} -  </p>
+        </marquee>
+      </div>
+    );
   }
 }
 
-export default News
-
+export default News;
