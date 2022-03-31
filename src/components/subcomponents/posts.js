0bd8a5c1569087aee17a6288d00fa05de746cc91
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-03-31 13:38:27 +0100

    Updated posts so index decrements instead of increments
    This allows posts to be rendered in order of how new they are.

--- src/components/subcomponents/posts.js
+++ src/components/subcomponents/posts.js
@@ -1,5 +1,6 @@
 //Pulls posts from CMS
 //Displays posts one at a time on a loop.
+//KNOWN ISSUES - first post on the CMS doesn't display. Index logic needs to be rewritten to allow an index of 0 without causing an error.
 
 import React, { useEffect, useState } from "react";
 import { Loader } from "../functional/Loader";
@@ -11,34 +12,39 @@ export const Anim = () => {
     //polls for updates every second
     pollInterval: 1000,
   });
-  //Index keeps track of which post is being displayed
-  const [index, setIndex] = useState(0);
   //limit ensures index is never higher than the amount of posts available from the CMS.
   let limit = 0;
+  //Index keeps track of which post is being displayed. Set state to limit
+  const [index, setIndex] = useState(limit);
+  
 
-  //Index is incremented within useEffect hook
+  //Index is decremented within useEffect hook
   useEffect(() => {
-    if (index <= limit) {
+    if (index <= 0 ) {
+      setIndex(limit);
+   
+    } else {
       const intervalId = setInterval(
-        () => setIndex((index) => index + 1),
+        () => setIndex((index) => index - 1),
         10000 // post displays for 10 seconds
       );
       return () => clearTimeout(intervalId);
-    } else {
-      setIndex((index) => 0);
     }
   }, [index, limit]);
 
   if (loading) return <Loader />;
   if (error) return `Error! ${error.message}`;
   else {
-    console.log(data.posts);
-    limit = data.posts.length -2
+    //set limit only if posts have been successfully pulled.
+    limit = data.posts.length -1
+    //map posts to array
     let body = data.posts[index].content.document.map((document) =>
       document.children.map((children) => children.text)
     );
-
-    console.log(data.posts.length);
+console.log(body)
+console.log(limit)
+console.log(index)
+   //render
     return <h1 className="fade-in-down">{body.join(" ")}</h1>;
   }
 };
