c259bd065f3219353de1e4fc7758575550b1e3a7
Liam Doyle <limegorilla@gmail.com>
2022-04-26 12:01:54 +0100

    Convert NextAuth/Prisma to new project

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import GitHubProvider from 'next-auth/providers/github';
+import AzureADProvider from "next-auth/providers/azure-ad";
 import prisma from '../../../lib/prisma';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
@@ -9,9 +9,10 @@ export default authHandler;
 
 const options = {
     providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+        AzureADProvider({
+            clientId: process.env.AZURE_AD_CLIENT_ID,
+            clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+            tenantId: process.env.AZURE_AD_TENANT_ID,
         }),
     ],
     adapter: PrismaAdapter(prisma),
