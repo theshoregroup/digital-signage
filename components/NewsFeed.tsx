5588304a0cacafc28cffbfd2c2543bfa6181d8d2
Liam Doyle <limegorilla@gmail.com>
2022-05-12 14:21:42 +0100

    Improve the way IT messages are displayed

--- components/NewsFeed.tsx
+++ components/NewsFeed.tsx
@@ -8,29 +8,27 @@ export default function NewsFeed() {
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>Loading...</div>;
 
-  let itMessage
 
-  if (data.itMessage !== undefined) {
-    itMessage = (
-      <div className="flex p-2 mb-5 bg-white drop-shadow-md rounded-lg">
-        <span className="flex h-5 w-5 relative mr-3 mt-1">
-          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-          <span className="relative inline-flex rounded-full h-5 w-5 bg-red-500"></span>
-        </span>
-        <div className="overflow-hidden">
-          <span className="text-3xl font-bold uppercase truncate">
-            {data.itMessage.type}: {data.itMessage.title}
-          </span>
-          <p className="text-xl italic flex-wrap">{data.itMessage.message}</p>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <aside className="">
-      {itMessage}
       <ul className="space-y-2">
+        {data.itMessage.map((item: any) => (
+          <li className="flex p-2 mb-5 bg-white drop-shadow-md rounded-lg">
+            <span className="flex h-5 w-5 relative mr-3 mt-1">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-5 w-5 bg-red-500"></span>
+            </span>
+            <div className="overflow-hidden">
+              <span className="text-3xl font-bold uppercase truncate">
+                {item.type}: {item.title}
+              </span>
+              <p className="text-xl italic flex-wrap">
+                {item.message}
+              </p>
+            </div>
+          </li>
+        ))}
         {data.news.map((item: any) => (
           <li className="flex" key={item.title}>
             <div className="h-full my-auto ml-1 mr-4 flex-shrink-0">
