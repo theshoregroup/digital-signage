967325f59537360d78727efa1c22a84ab2ebac33
Liam Doyle <limegorilla@gmail.com>
2022-05-12 18:40:29 +0100

    Comments on how to handle multiple IT Messages

--- pages/api/admin/itMsg/index.ts
+++ pages/api/admin/itMsg/index.ts
@@ -2,10 +2,7 @@ import prisma from "../../../../lib/prisma";
 
 export default async (req: any, res: any) => {
     if (req.method === "GET") {
-        // By default assume that we will return all messages
-        if (req.query.range) {
-           // TODO
-        }
+        // TODO: Decide if I want to also have an option to return a scoped list of messages (i.e only IT Messages published between x and y, or only messages that have a certain status)
         return res.status(200).json(await prisma.itMessage.findMany());
     } else if (req.method === "POST") {
         const { title, message, beginsAt, endsAt, contact } = req.body;
