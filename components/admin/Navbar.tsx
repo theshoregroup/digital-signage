5f6192d58fb49cd575a0b67151a1f4347ad79152
Liam Doyle <limegorilla@gmail.com>
2022-05-13 15:51:39 +0100

    Add a navbar for the Admin UI

--- components/admin/Navbar.tsx
+++ components/admin/Navbar.tsx
@@ -1,8 +1,45 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function AdminNavbar() {
   const { data: session, status } = useSession();
   return (
-  <nav></nav>
+    <nav className="w-screen h-20 bg-slate-600 px-4 text-white">
+      <div className="flex justify-between items-center h-20">
+        <div className="flex items-center">
+          <Link href="/">
+            <a className="flex items-center">
+              <img
+                src="/images/logo.png"
+                alt="Digital Signage"
+                className="h-8 w-8"
+              />
+              <span className="ml-2 hidden sm:inline-block">
+                Digital Signage
+              </span>
+            </a>
+          </Link>
+        </div>
+        <div className="flex items-center text-right">
+          {status === "unauthenticated" && (
+            <button onClick={() => signIn()}>Sign in!</button>
+          )}
+          {status === "authenticated" && (
+            <div className="space-x-3">
+              <span className="hidden sm:inline-block">
+                Welcome back, {session.user?.name}
+              </span>
+
+              <button
+                className="p-2 bg-white text-black rounded-xl"
+                onClick={() => signOut()}
+              >
+                Sign Out
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 }
