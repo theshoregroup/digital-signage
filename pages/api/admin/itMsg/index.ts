c4488fbf6e2f9c5945d6e56f394de68bfc51cc6f
Liam Doyle <limegorilla@gmail.com>
2022-05-30 15:27:34 +0100

    Added logic for deleting all messages

--- pages/api/admin/itMsg/index.ts
+++ pages/api/admin/itMsg/index.ts
@@ -2,7 +2,7 @@ import prisma from "../../../../lib/prisma";
 
 export default async (req: any, res: any) => {
     if (req.method === "GET") {
-        // TODO: Decide if I want to also have an option to return a scoped list of messages (i.e only IT Messages published between x and y, or only messages that have a certain status)
+
         return res.status(200).json(await prisma.itMessage.findMany());
     } else if (req.method === "POST") {
         const { title, message, beginsAt, endsAt, contact } = req.body;
@@ -16,5 +16,10 @@ export default async (req: any, res: any) => {
             },
         });
         return res.status(200).json(newMessage);
+    } else if (req.method === "DELETE") {
+        if (req.body["delete-type"] === "all") {
+            await prisma.itMessage.deleteMany({});
+            return res.status(200).json({ message: "All IT Messages deleted" });
+        }
     }
 }
