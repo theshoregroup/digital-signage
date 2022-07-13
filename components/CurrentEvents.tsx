9f09e1433e6c294bb24523b41ddeef951c0bb5e6
Liam Doyle <limegorilla@gmail.com>
2022-07-13 12:10:30 +0100

    Improve Current Events

--- components/CurrentEvents.tsx
+++ components/CurrentEvents.tsx
@@ -4,45 +4,77 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function CurrentEvents() {
-  const { data, error } = useSWR("/api/getNews", fetcher, {
-    refreshInterval: 10000,
-    refreshWhenHidden: true,
-  });
+  function itMessaging() {
+    const { data, error } = useSWR("/api/getNews", fetcher, {
+      refreshInterval: 10000,
+      refreshWhenHidden: true,
+    });
 
-  if (error || data == undefined) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
+    if (error || data == undefined)
+      return <div>IT Messages: Failed to load</div>;
+    if (!data) return <div>Loading...</div>;
 
-  return (
-    <aside className="">
-      <ul className="space-y-2">
+    return (
+      <>
         {data.itMessage.map((item: any) => (
-          <li className="flex p-2 mb-5 bg-white drop-shadow-md rounded-lg">
+          <li
+            key={item.title}
+            className="flex p-2 mb-5 bg-white drop-shadow-md rounded-lg"
+          >
             <span className="flex h-5 w-5 relative mr-3 mt-1">
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-5 w-5 bg-red-500"></span>
             </span>
             <div className="overflow-hidden">
-              <span className="text-3xl font-bold truncate">{item.title}</span>
+              <span className="text-3xl font-bold w-full flex-wrap">
+                {item.title}
+              </span>
               <p className="text-xl italic flex-wrap">{item.message}</p>
             </div>
           </li>
         ))}
-        {data.news.map((item: any) => (
+      </>
+    );
+  }
+
+  function news() {
+    const { data, error } = useSWR("/api/news", fetcher, {
+      refreshInterval: 1000000,
+      refreshWhenHidden: true,
+    });
+
+    if (error) return <div>Failed to load</div>;
+    if (!data) return <div>Loading...</div>;
+
+    return (
+      <>
+        {data.articles.map((item: any) => (
           <li className="flex" key={item.title}>
             <div className="h-full my-auto ml-1 mr-4 flex-shrink-0">
               <img
-                src={item.image || "https://placeholder.pics/svg/30x30"}
+                src={item.urlToImage || "https://placeholder.pics/svg/30x30"}
                 alt={item.title}
+                onError={(e) => {
+                  e.currentTarget.src = "https://placeholder.pics/svg/30x30";
+                }}
+                className="w-10"
               />
             </div>
             <div className="overflow-hidden flex-shrink">
-              <span className="text-2xl font-bold block truncate">
-                {item.title}
-              </span>
-              <span className="text-xl block truncate">{item.source}</span>
+              <span className="text-xl font-bold block">{item.title}</span>
+              <span className="text-xl block truncate">{item.source.name}</span>
             </div>
           </li>
         ))}
+      </>
+    );
+  }
+
+  return (
+    <aside className="">
+      <ul className="space-y-4">
+        {itMessaging()}
+        {news()}
       </ul>
     </aside>
   );
