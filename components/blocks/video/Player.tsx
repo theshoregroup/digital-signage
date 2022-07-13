759d2003fd70358287658a17cb7f0ab11b508c79
Liam Doyle <limegorilla@gmail.com>
2022-07-13 13:12:21 +0100

    Add default player

--- components/blocks/video/Player.tsx
+++ components/blocks/video/Player.tsx
@@ -1,20 +1,42 @@
 // Produces a client-side video player.
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import videojs from "video.js";
 
-export default function Player() {
+export const DefaultVideoPlayer = (props: any) => {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+  const { options, onReady } = props;
+
   useEffect(() => {
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-      console.log("Video.js Ready", this);
-    });
-  });
+    if (!playerRef.current) {
+      const videoElement = videoRef.current;
+
+      if (!videoElement) return;
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        videojs.log("Player Ready");
+        onReady && onReady(player);
+      }));
+    }
+  }, [options, videoRef]);
+
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
   return (
     <div data-vjs-player>
-      <video
-        ref={(node) => (this.videoNode = node)}
-        className="video-js"
-      ></video>
+      <video ref={videoRef} className="video-js vjs-big-play-centered" />
     </div>
   );
-}
+};
+
+export default DefaultVideoPlayer;
