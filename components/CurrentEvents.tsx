f2b442a42d1d4241f6ccd216d11632f2b849b8ac
Liam Doyle <limegorilla@gmail.com>
2022-07-28 14:45:04 +0100

    Add news error component

--- components/CurrentEvents.tsx
+++ components/CurrentEvents.tsx
@@ -1,3 +1,4 @@
+import { NewspaperIcon } from "@heroicons/react/solid";
 import useSWR from "swr";
 
 // @ts-ignore: Rest parameter 'args' implicitly has an 'any[]' type.
@@ -29,7 +30,9 @@ export default function CurrentEvents() {
               <span className="text-4xl font-bold w-full flex-wrap">
                 {item.title}
               </span>
-              <p className="text-3xl italic flex-wrap">{item.message}</p>
+              <p className="text-3xl italic font-semibold flex-wrap">
+                {item.message}
+              </p>
             </div>
           </li>
         ))}
@@ -43,7 +46,28 @@ export default function CurrentEvents() {
       refreshWhenHidden: true,
     });
 
-    if (error) return <div>Failed to load</div>;
+    if (error)
+      return (
+        <div className="grid place-items-center">
+          <div className="text-red-300 p-5 rounded-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-44 w-44"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={1.7}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+              />
+            </svg>
+            <span className="text-2xl font-bold">Error</span>
+          </div>
+        </div>
+      );
     if (!data) return <div>Loading...</div>;
 
     return (
